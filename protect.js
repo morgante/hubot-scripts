b60f055b43a32ec049c46766880937cd3b9029c4
Morgante Pell <morgante.pell@morgante.net>
2013-07-18 14:47:27 -0400

    bidirectional karma

--- protect.js
+++ protect.js
@@ -21,12 +21,21 @@ var favorites = [
 ]
 
 function giveKarma( who, karma, robot ) {
+		
+	if( karma < 0 )
+	{
+		msg = who + '--';
+		karma++;
+	}
+	else
+	{
+		msg = who + '++'
+		karma--
+	}
 	
-	robot.send( { user: { name: 'manyabot' } }, 'Just correcting a gross injustice! ' + who + '++' );
+	robot.send( { user: { name: 'manyabot' } }, msg );
 	
-	karma--;
-	
-	if( karma > 0 )
+	if( karma != 0 )
 	{
 		setTimeout( giveKarma, 10000, who, karma, robot );
 	}
@@ -39,7 +48,7 @@ module.exports = function(robot) {
     
 	robot.hear(/(\S+[^-:\s])[: ]*--(\s|$)/, function(msg) {
         
-        var who = msg.match[1].toLowerCase();
+      var who = msg.match[1].toLowerCase();
         
 	    if( _.indexOf( favorites, who ) != -1 )
 	    {
@@ -48,9 +57,15 @@ module.exports = function(robot) {
 	    	    
 	});
 	
-	robot.hear(/give (\S+[^-:\s])[: ]* (\d+) karma(\s|$)/, function(msg) {
+	robot.respond(/give (\S+[^-:\s])[: ]* (\d+) karma(\s|$)/, function(msg) {
 				
 		giveKarma( msg.match[1], msg.match[2], robot );
+	    	    
+	});
+	
+	robot.respond(/take (\d+) karma from (\S+[^-:\s])[: ]*(\s|$)/, function(msg) {
+				
+		giveKarma( msg.match[2], -msg.match[1], robot );
 	    	    
 	});
 
